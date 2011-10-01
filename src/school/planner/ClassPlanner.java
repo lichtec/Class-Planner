42937910d339d334a91dc93442f04d963ced2732
Elizabeth Unrein <eunrein@gmail.com>
2011-09-30 21:22:53 -0500

    Working program files! This is a android app that creates a single tab with a text field.

---
+++ src/school/planner/ClassPlanner.java
@@ -0,0 +1,30 @@
+package school.planner;
+import school.planner.R;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+
+public class ClassPlanner extends TabActivity
+{
+	public void onCreate(Bundle savedInstanceState) //This method creates a premade tab.
+	{
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+	    //Resources res = getResources(); // Resource object to get Drawables //if we want icons for the tabs.
+	    TabHost tabHost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+	    Intent semIntent;
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    semIntent = new Intent();
+	    semIntent.setClass(this, SemesterActivity.class);
+
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabHost.newTabSpec("Fall '11").setIndicator("Fall '11").setContent(semIntent);
+	    tabHost.addTab(spec);
+	    
+	    tabHost.setCurrentTab(2); //Probably useful later.
+	}
+}
---
+++ src/school/planner/SemesterActivity.java
@@ -0,0 +1,15 @@
+package school.planner;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SemesterActivity extends Activity
+{
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		TextView tempText = new TextView(this);
+		tempText.setText("Please add functionality to me :D");
+		setContentView(tempText);
+	}
+}
